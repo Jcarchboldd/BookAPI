a61d3e355d21d1b5cedc9964fa36fc8285325a02
Jair <jcarchboldd@outlook.com>
2025-04-24 10:49:46 +0200

    Add Book Respository

---
+++ BookAPI/Infrastructure/Repositories/BookRepository.cs
@@ -0,0 +1,49 @@
+using BookAPI.Infrastructure.Data;
+
+namespace BookAPI.Infrastructure.Repositories;
+
+public class BookRepository(BookDbContext context) : IBookRepository
+{
+    public async Task<IEnumerable<Book>> GetAllBooksAsync()
+    {
+        return await context.Books
+            .Include(b => b.Reviews)
+            .ToListAsync();
+    }
+
+    public async Task<Book?> GetBookByIdAsync(Guid id)
+    {
+        return await context.Books
+            .Include(b => b.Reviews)
+            .FirstOrDefaultAsync(b => b.Id == id);
+            
+    }
+
+    public async Task CreateBookAsync(Book book)
+    {
+        await context.Books.AddAsync(book);
+    }
+
+    public async Task UpdateBookAsync(Book book)
+    {
+        var existingBook = await context.Books.FindAsync(book.Id);
+        if (existingBook is null)
+        {
+            //TODO: Replace with custom NotFoundException
+            throw new Exception();
+        }
+
+        context.Entry(existingBook).CurrentValues.SetValues(book);
+    }
+
+    public async Task DeleteBookAsync(Guid id)
+    {
+        var existingBook = await context.Books.FindAsync(id);
+        if (existingBook is null)
+        {
+            throw new Exception();
+        }
+        
+        context.Books.Remove(existingBook);
+    }
+}
---
+++ BookAPI/Infrastructure/Repositories/IBookRepository.cs
@@ -0,0 +1,10 @@
+namespace BookAPI.Infrastructure.Repositories;
+
+public interface IBookRepository
+{
+    Task<IEnumerable<Book>> GetAllBooksAsync();
+    Task<Book?> GetBookByIdAsync(Guid id);
+    Task CreateBookAsync(Book book);
+    Task UpdateBookAsync(Book book);
+    Task DeleteBookAsync(Guid id);
+}

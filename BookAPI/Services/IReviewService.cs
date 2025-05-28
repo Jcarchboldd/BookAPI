c9630eb6d924ebdac6d4801a674fbde4703dc7bd
Jair <jcarchboldd@outlook.com>
2025-05-28 23:26:41 +0200

    Add Review Service

--- BookAPI/Services/IReviewService.cs
+++ BookAPI/Services/IReviewService.cs
@@ -3,7 +3,7 @@ namespace BookAPI.Services;
 public interface IReviewService
 {
     Task<IEnumerable<ReviewResponse>> GetAllReviewsAsync();
-    Task<ReviewResponse?> GetReviewAsync(int id);
+    Task<ReviewResponse?> GetReviewAsync(Guid id);
     Task<Guid> CreateReviewAsync(CreateReviewRequest request);
     Task UpdateReviewAsync(UpdateReviewRequest request);
     Task DeleteReviewAsync(Guid id);
---
+++ BookAPI/Services/ReviewService.cs
@@ -0,0 +1,38 @@
+namespace BookAPI.Services;
+
+public class ReviewService(IUnitOfWork unitOfWork, IServiceProvider serviceProvider) : IReviewService
+{
+    public async Task<IEnumerable<ReviewResponse>> GetAllReviewsAsync()
+    {
+        var result = await unitOfWork.ReviewRepository.GetAllReviewsAsync();
+        
+        return result.Adapt<IEnumerable<ReviewResponse>>();
+    }
+
+    public async Task<ReviewResponse?> GetReviewAsync(Guid id)
+    {
+        var result = await unitOfWork.ReviewRepository.GetReviewByIdAsync(id);
+        
+        return result.Adapt<ReviewResponse>();
+    }
+
+    public async Task<Guid> CreateReviewAsync(CreateReviewRequest request)
+    {
+        var review = request.Adapt<Review>();
+        await unitOfWork.ReviewRepository.CreateReviewAsync(review);
+        await unitOfWork.SaveAsync();
+        return review.Id;
+    }
+
+    public async Task UpdateReviewAsync(UpdateReviewRequest request)
+    {
+        var review = request.Adapt<Review>();
+        await unitOfWork.ReviewRepository.UpdateReviewAsync(review);
+        await unitOfWork.SaveAsync();
+    }
+
+    public async Task DeleteReviewAsync(Guid id)
+    {
+        await unitOfWork.ReviewRepository.DeleteReviewAsync(id);
+    }
+}

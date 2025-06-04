6ac7c2778bff1d4cc5ed7e7311a47f22c2c181b2
Jair <jcarchboldd@outlook.com>
2025-06-04 22:20:21 +0200

    Add GetBookReviews Controller

--- BookAPI/Controllers/ReviewController.cs
+++ BookAPI/Controllers/ReviewController.cs
@@ -4,5 +4,11 @@ namespace BookAPI.Controllers;
 [Route("api/[controller]")]
 public class ReviewController(IReviewService reviewService) : ControllerBase
 {
-    
+    [HttpGet("book/{bookId:guid}")]
+    public async Task<IActionResult> GetBookReviewsAsync(Guid bookId)
+    {
+        var bookReviews = await reviewService.GetAllReviewsAsync(bookId);
+        
+        return Ok(bookReviews);
+    }
 }

8fdaf5c39e9333057e46e6ad608d00d57d359b36
Jair <jcarchboldd@outlook.com>
2025-06-05 22:31:56 +0200

    Add GetReviewByIdAsync Endpoint

--- BookAPI/Controllers/ReviewController.cs
+++ BookAPI/Controllers/ReviewController.cs
@@ -4,11 +4,22 @@ namespace BookAPI.Controllers;
 [Route("api/[controller]")]
 public class ReviewController(IReviewService reviewService) : ControllerBase
 {
-    [HttpGet("book/{bookId:guid}")]
+    [HttpGet("bookReviews{bookId:guid}")]
     public async Task<IActionResult> GetBookReviewsAsync(Guid bookId)
     {
         var bookReviews = await reviewService.GetAllReviewsAsync(bookId);
         
         return Ok(bookReviews);
+    }
+
+    [HttpGet("{reviewId:guid}", Name = nameof(GetReviewByIdAsync))]
+    public async Task<IActionResult> GetReviewByIdAsync(Guid reviewId)
+    {
+        var result = await reviewService.GetReviewByIdAsync(reviewId);
+        if (result == null)
+        {
+            return NotFound();
+        }
+        return Ok(result);
     }
 }
